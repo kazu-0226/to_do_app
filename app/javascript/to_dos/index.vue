60caa7356d7c3b6aed096f0e1e94035c38b2f192
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-10-04 11:54:40 +0000

    [Add]終了タスクの表示分け

--- app/javascript/to_dos/index.vue
+++ app/javascript/to_dos/index.vue
@@ -1,7 +1,9 @@
 <template>
+<el-tabs v-model= "activeName">
+  <el-tab-pane label="ToDo" name="toDo">
   <!-- :data= "dataオプションで定義したtoDosオブジェクトでテーブルに表示する -->
   <el-table
-    :data= "toDos"
+    :data= "filter(toDos, false)"
     style= "width: 100%">
     <el-table-column
       prop="finished">
@@ -34,12 +36,53 @@
     </template>
     </el-table-column>
   </el-table>
+   </el-tab-pane>
+  <el-tab-pane label= "終了したToDo" name="finishedToDo">
+    <el-table
+    :data= "filter(toDos, true)"
+    style= "width: 100%">
+    <el-table-column
+      prop="finished">
+      <!-- v-slotを設定し、それぞれの行のToDoのデータにアクセス -->
+      <template v-slot="scope">
+        <!-- v-modelディレクティブを使用し、ここにToDoのfinishedカラムを設定 -->
+        <!-- el-checkboxにchangeディレクティブを定義してチェックボックスが変更されると、updateToDoメソッドを呼び出す -->
+        <el-checkbox
+          v-model= "scope.row.finished"
+          @change= "updateToDo(scope.row.id, scope.row.finished)" ></el-checkbox>
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop= "title">
+    </el-table-column>
+    <el-table-column
+      prop= "expired_at">
+    </el-table-column>
+    <el-table-column
+        width= "120">
+    <!-- templateタグでくくって、templateタグにv-slotを指定することで、それぞれのToDoにアクセス -->
+    <template v-slot="scope">
+    <!-- v-onディレクティブでクリックした時にdestroyToDoメソッドを実行 -->
+    <!-- scope.row.idでToDoのデータにアクセス -->
+    <el-button
+        @click= "destroyToDo(scope.row.id)"
+        type= "danger"
+        icon= "el-icon-delete"
+        circle></el-button>
+    </template>
+    </el-table-column>
+  </el-table>
+  </el-tab-pane>
+</el-tabs>
 </template>
+
+
 <script>
 // Vue.jsでAjaxを行うために、app/javascript/to_dos/index.vueでaxiosを利用する
 import axios from 'axios'
-// lodashのrejectメソッドを利用
-import {reject} from 'lodash';
+// タスクの削除のため、lodashのrejectメソッドを利用
+// 終了タスクの振り分けのため、lodashのfilterメソッドを利用
+import {reject, filter} from 'lodash';
   export default {
 // dataオプションでtoDosオブジェクトを定義しその値を空の配列に
   data() {
@@ -75,8 +118,12 @@ import {reject} from 'lodash';
       if (res.status === 200) {
         console.log(res)
       }
-  })
-}
-}
+    })
+  },
+  // filterメソッドを使って引数に与えられたToDoの一覧から、引数に与えられたfinishedの条件に合うToDoだけを返す
+  filter(toDos, finished) {
+  return filter(toDos, ['finished', finished])
+  }
+ }
 }
 </script>
